82b2bc653e5a0bd0bae6754fd3f344d6689036a9
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-07-15 18:24:10 +0200

    Hit the Lottery

---
+++ Mohamed Ayman repo/level-1/codeforces-phase-1-1/9_Hit_the_Lottery.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::min;
+using std::string;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << (n / 100) + (n % 100) / 20 + ((n % 100) % 20) / 10 + (((n % 100) % 20) % 10) / 5 + ((((n % 100) % 20) % 10) % 5);
+    return 0;
+}
