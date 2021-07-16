4b75793ab03c6928dcad325ac8a8cf85de6deb42
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-07-16 03:54:21 +0200

    Splits

---
+++ Mohamed Ayman repo/level-1/codeforces-phase-1-1/11_Splits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::min;
+using std::string;
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<n/2+1;
+    return 0;
+}
