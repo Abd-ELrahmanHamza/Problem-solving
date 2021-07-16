7e206d7f71f7579d0c00e987d4817a99ef27ca43
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-07-16 20:04:35 +0200

    Die Roll

---
+++ Mohamed Ayman repo/level-1/codeforces-phase-1-1/15_Die_Roll.cpp
@@ -0,0 +1,26 @@
+//Die Roll
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::min;
+using std::max;
+using std::string;
+int GCD(int a,int b)
+{
+    if(b==0)
+        return a;
+    return GCD(b, a % b);
+}
+int main()
+{
+    int y,w;
+    cin>>y>>w;
+    int gcd = GCD(7-max(y,w),6);
+    cout<<(7-max(y,w))/gcd<<"/"<<6/gcd;
+    return 0;
+}
