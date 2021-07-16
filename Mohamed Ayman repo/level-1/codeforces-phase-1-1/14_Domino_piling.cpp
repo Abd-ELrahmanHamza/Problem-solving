bdb2d8c5fcbf817fdbb567f48f20e733183753d3
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-07-16 19:46:07 +0200

    Domino piling

---
+++ Mohamed Ayman repo/level-1/codeforces-phase-1-1/14_Domino_piling.cpp
@@ -0,0 +1,19 @@
+//Domino piling
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::min;
+using std::string;
+
+int main()
+{
+    int l, w;
+    cin >> l >> w;
+    cout << (l * w) / 2;
+    return 0;
+}
