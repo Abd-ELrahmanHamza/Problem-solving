05fcca946bd160694f96352162ef59fcb156342d
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-07-24 19:16:49 +0200

    Is it rated?

---
+++ Mostafa saad sheet a/052_Is_it_rated.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+int main()
+{
+    int n;
+    pair<int, int> NewPair, OldPair;
+    cin >> n;
+    cin >> OldPair.first >> OldPair.second;
+    bool IsRated = false, IsInc = true;
+    if (OldPair.first != OldPair.second)
+        IsRated = true;
+
+    for (int i = 1; i < n; i++)
+    {
+        cin >> NewPair.first >> NewPair.second;
+        if (NewPair.first != NewPair.second)
+        {
+            IsRated = true;
+        }
+        if (NewPair.first > OldPair.first)
+            IsInc = false;
+        OldPair = NewPair;
+    }
+    if (IsRated)
+        cout << "rated";
+    else if (!IsInc)
+        cout << "unrated";
+    else
+        cout << "maybe";
+    return 0;
+}
