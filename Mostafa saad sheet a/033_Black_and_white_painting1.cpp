53ea49905e74705a8b35c6580ccf45e2e835f89d
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-07-24 08:00:18 +0200

    Black and white painting 1

---
+++ Mostafa saad sheet a/033_Black_and_white_painting1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::max;
+using std::min;
+using std::pair;
+using std::string;
+using std::vector;
+int main()
+{
+    int m,n,c;
+    cin>>n>>m>>c;
+    cout <<((n-7)*(m-7)+c)/2 ;
+    return 0;
+}
