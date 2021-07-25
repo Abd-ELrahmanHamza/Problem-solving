340dc4b2b4605e574ab14a7a0b48bf41d75c66ae
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-07-25 23:52:56 +0200

    Calculating Function

---
+++ Mostafa saad sheet a/058_Calculating_Function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::max;
+using std::min;
+using std::pair;
+using std::string;
+using std::vector;
+int main()
+{
+    long long n;
+    cin >> n;
+    cout << n / 2 - (n % 2 != 0 ? n : 0);
+    return 0;
+}
