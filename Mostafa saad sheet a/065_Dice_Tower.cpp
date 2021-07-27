c344c03fa4a10576dc155d1f19a9fcf20de8a89b
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-07-27 06:12:37 +0200

    Dice Tower

---
+++ Mostafa saad sheet a/065_Dice_Tower.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+int main()
+{
+    int n, top;
+    bool flag = true;
+    cin >> n >> top;
+    pair<int, int> faces;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> faces.first >> faces.second;
+        if (faces.first == top || faces.first == 7 - top || faces.second == top || faces.second == 7 - top)
+        {
+            flag = false;
+            break;
+        }
+    }
+    if (flag)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
